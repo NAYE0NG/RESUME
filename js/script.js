afc5b13ff1897ed60a3c9603966c1d04596379fc
NAYEONG_PC\NAYEONG <nayeong_e@naver.com>
2018-03-03 05:57:19 +0900

    상단 메뉴바 고정, 프로젝트 마우스오버 이벤트 설정

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var currentScrollTop = 0, footerScrollTop=0 , menuScrollTop = 0;
+
+window.onload = function() {
+    menuScrollTop =$('#menu2').offset().top;
+    footerScrollTop = $('footer').offset().top;
+    //새로고침
+    scrollController();
+    // 스크롤 하는 경우 실행됨
+    $(window).on('scroll', function() {
+        scrollController();
+    });
+}
+
+function scrollController() {
+    currentScrollTop = $(window).scrollTop();
+    if(currentScrollTop > menuScrollTop && currentScrollTop<footerScrollTop-190){
+        $('#menu2').addClass('fixed');    
+        $('#section1-inner').css('margin-top','95px');
+    }else{ 
+        $('#menu2').removeClass('fixed');
+        $('#section1-inner').css('margin-top','0px');
+    }
+}
